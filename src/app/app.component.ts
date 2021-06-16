fe09dabcf7c26a587d422ddc397ddb9a2d5cf9a3
Shan <70942213+ShanAZIZ@users.noreply.github.com>
2021-06-17 01:32:44 +0200

    Requete des Employees sur main component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Employee } from './employee';
+import { EmployeeService } from './employee.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'employeemanager-app';
+export class AppComponent implements OnInit{
+  public employees: Employee[] = [];
+
+  constructor(private employeeService: EmployeeService) {}
+
+  ngOnInit() {
+    this.getAllEmployees();
+  }
+
+  public getAllEmployees(): void {
+    this.employeeService.getAllEmployees()
+    .subscribe(
+      (response: Employee[]) => {
+        this.employees = response;
+      }, 
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+  
 }
--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -2,18 +2,19 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { Employee } from './employee';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
 
-  private apiServerUrl = '';
+  private apiServerUrl = environment.apiBaseUrl;
 
   constructor(private http: HttpClient ) {}
 
-  public getAllEmployees(): Observable<Employee> { 
-    return this.http.get<Employee>(this.apiServerUrl + '/employee/all')
+  public getAllEmployees(): Observable<Employee[]> { 
+    return this.http.get<Employee[]>(this.apiServerUrl + '/employee/all')
   }
   
   public getEmployee(employeeId: number): Observable<Employee> { 
